f2fd831c256d05080d3ba02d822988105edcccd3
DilanMDA <akiladilan04@gmail.com>
2023-01-24 01:00:16 +0530

    end of vehicles section

--- js/script.js
+++ js/script.js
@@ -48,3 +48,30 @@ document.querySelector(".home").onmouseleave = (e) => {
     elm.style.transform = `translateX(0px) translateY(0px)`;
   });
 };
+
+// Initialize Swiper
+var swiper = new Swiper(".vehicles-slider", {
+  grabCursor: true,
+  centeredSlides: true,
+  spaceBetween: 20,
+  loop: true,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
